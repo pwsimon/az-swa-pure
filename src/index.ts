c40fe2dc4429fc7e0fee1edf27a9a1ef194ed109
Peter Simon <psi@estos.de>
2026-01-25 18:35:58 +0100

    add (Backend) Route

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,12 @@ app.get("/", (req: Request, res: Response) => {
 
 // API-Route built/coded for: requires authorization using middleware Easy Auth
 app.get("/api/authreq", (req: Request, res: Response) => {
-	// [x-ms-client-principal](https://learn.microsoft.com/de-de/azure/static-web-apps/user-information?tabs=javascript#api-functions)
-	// Hint: Der x-ms-client-principal-Header, auf den in der API-Funktion zugegriffen werden kann, enthält das claims-Array nicht.
+	/*
+	* [x-ms-client-principal](https://learn.microsoft.com/de-de/azure/static-web-apps/user-information?tabs=javascript#api-functions)
+	* Hints:
+	* Der x-ms-client-principal-Header, auf den in der API-Funktion zugegriffen werden kann, enthält das claims-Array nicht.
+	* Wir sehen hier in: userRoles auch die Rollen, die dem Benutzer per Azure Role Assignment zugewiesen wurden (rollenverwaltung.jpg).
+	*/
 	const header = req.headers['x-ms-client-principal'] as string;
 	if (header.length === 0)
 		return res.status(401).send("Unauthorized");
@@ -21,7 +25,7 @@ app.get("/api/authreq", (req: Request, res: Response) => {
 	const clientPrincipal = JSON.parse(decoded);
 
 	// [Retrieve tokens in app code](https://learn.microsoft.com/en-us/azure/app-service/configure-authentication-oauth-tokens#retrieve-tokens-in-app-code)
-	res.json({ result: "OK", userId: clientPrincipal.userId });
+	res.json({ result: "OK", userRoles: clientPrincipal.userRoles, userId: clientPrincipal.userId });
 });
 
 // der Port: 7071 ist der default port fuer die: azure functions core tools
