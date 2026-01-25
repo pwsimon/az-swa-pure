c66b75bef7a19136586313531e7bc69cf93ac223
Peter Simon <psi@estos.de>
2026-01-25 16:59:04 +0100

    add routes and its needed roles

--- public/index.js
+++ public/index.js
@@ -13,3 +13,18 @@ function doBackendCall() {
 			console.error("Error calling backend:", error);
 		});
 }
+window.addEventListener("load", () => {
+	fetch(document.location.origin + "/.auth/me")
+		.then(response => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response.json();
+		})
+		.then(data => {
+			console.log("User Roles:", data.clientPrincipal?.userRoles);
+		})
+		.catch(error => {
+			console.error("Error fetching user info:", error);
+		});
+});
